df52e8fb8f7e0568e8b7c4da4ef9c1f778b40ed9
AungKhantHein314 <aungkhanthein314@gmail.com>
2022-05-07 22:01:59 +0630

    deploy

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDatabase from "./config/MongoDb.js";
+import ImportData from "./DataImport.js";
+import productRoute from "./Routes/ProductRoutes.js";
+import { errorHandler, notFound } from "./Middleware/Errors.js";
+import userRouter from "./Routes/UserRoutes.js";
+import orderRouter from "./Routes/orderRoutes.js";
+import cors from "cors";
+
+dotenv.config();
+connectDatabase();
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+// API
+app.get("/", (req, res) => {
+  res.send("OK")
+})
+app.use("/api/import", ImportData);
+app.use("/api/products", productRoute);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
+
+// ERROR HANDLER
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server run in port ${PORT}`));
