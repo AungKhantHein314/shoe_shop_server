923f1aac4b40aefd6260b53f8ac8aadae9af4c61
Aung Khant Hein <104364740+AungKhantHein314@users.noreply.github.com>
2022-05-29 08:07:44 +0630

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ import productRoute from "./Routes/ProductRoutes.js";
 import { errorHandler, notFound } from "./Middleware/Errors.js";
 import userRouter from "./Routes/UserRoutes.js";
 import orderRouter from "./Routes/orderRoutes.js";
-import cors from "cors";
 
 dotenv.config();
 connectDatabase();
 const app = express();
 app.use(express.json());
-app.use(cors())
 
 // API
 app.get("/", (req, res) => {
